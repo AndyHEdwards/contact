d6ca304c5ac500fe70b73ed02cd24d355f9519cd
Daniel Lewis <email": null>
2015-08-24 12:44:38 +1200

    Finished slice 2

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,8 @@ class SiteController < ApplicationController
   def index
     @title = "Welcome!"
   end
+
+  def about
+    @title = "About Us"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'site#index'
+
+  get '/about' => 'site#about'
+
 end
+
